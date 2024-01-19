cd361f8426d3011f341bb8a6c8853238a7dd26d9
leelulibo <leelulibo@gmail.com>
2024-01-19 18:55:27 +0200

    Done

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    $('.about-slider').slick({
+        dots: true, // Show navigation dots
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+});
